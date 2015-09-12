c7f8d4b92d5931c3e4dc4dadea4493aef01d3116
JillZhang <jianglu.zhang@gmail.com>
2015-09-12 19:58:50 -0400

    assignment1

--- plot2.R
+++ plot2.R
@@ -3,11 +3,12 @@ power<-read.table("household_power_consumption.txt",header=TRUE,sep = ";",na.str
 power.data<-subset(power,Date=="1/2/2007"|Date=="2/2/2007")
 #Convert the date and 
 power.data$Datetime<-paste(power.data$Date,power.data$Time,sep=" ")
-power.data$Time<-strptime(power.data$Datetime,format="%d/%m/%Y %H:%M:%S")
+power.data$Datetime<-strptime(power.data$Datetime,format="%d/%m/%Y %H:%M:%S")
 #save the result to png
-png(filename = "plot1.png",width=480,height=480)
+png(filename = "plot2.png",width=480,height=480)
 #Generate the plot
-hist(power.data$Global_active_power,col="red",
-     main="Global Active Power",
-     xlab="Global Active Power (kiowatts)")
+with(power.data,{
+  plot(Datetime,Global_active_power,type="l",ylab="Global Active Power (kilowatts)", main="", xlab="")
+}
+     )
 dev.off()
