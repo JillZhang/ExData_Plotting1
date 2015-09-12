5b4150862dbbb5c904b2bb40b6e8c4047ea52eb9
JillZhang <jianglu.zhang@gmail.com>
2015-09-12 18:30:25 -0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,9 +1,9 @@
 #Read Data
 power<-read.table("household_power_consumption.txt",header=TRUE,sep = ";",na.strings = "?",stringsAsFactors = FALSE)
 power.data<-subset(power,Date=="1/2/2007"|Date=="2/2/2007")
-#Convert the date and time
-power.data$Time<-strptime(power.data$Time,"%H:%M:%S")
-power.data$Date<-as.Date(power.data$Date,"%d/%m/%Y")
+#Convert the date and 
+power.data$Datetime<-paste(power.data$Date,power.data$Time,sep=" ")
+power.data$Time<-strptime(power.data$Datetime,format="%d/%m/%Y %H:%M:%S")
 #save the result to png
 png(filename = "plot1.png",width=480,height=480)
 #Generate the plot
